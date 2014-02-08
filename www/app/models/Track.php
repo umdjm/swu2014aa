5a23d12db6e1802e8dfadfce54a8b6be89288a85
Colleen Sain <collsain@umich.edu>
2014-02-08 12:43:15 -0500

    Added assoc for Track to Issue and User

--- www/app/models/Track.php
+++ www/app/models/Track.php
@@ -4,4 +4,15 @@ class Track extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	public function user()
+	{
+	    return $this->belongsTo('User');
+	}
+
+	public function issue()
+	{
+	    return $this->belongsTo('Issue');
+	}
+
 }
