94b17c3eb8744d73a71a823cc7ea79ecffb4afe3
vagrant <vagrant@laravel>
2014-02-08 16:37:55 +0000

    Created migration and model for Issue

---
+++ www/app/database/migrations/2014_02_08_154313_create_issues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateIssuesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('issues', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('photo');
+			$table->longText('desc');
+			$table->string('status');
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('issues');
+	}
+
+}
