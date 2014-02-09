6a8743542fcb5e1de04da47206ece4b7146ef8ad
mjeffery <michael.jeffery@gmail.com>
2014-02-08 19:01:49 -0500

    styled the users.create view

--- www/app/views/users/create.blade.php
+++ www/app/views/users/create.blade.php
@@ -1,15 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-<h1> Create a user here. </h1>
-
-<form method="POST" action="/users">
-	<label>email:</label>
-	<input type="email" name="email" placeholder="email address">
-	<label>password:</label>
-	<input type="password" name="password" placeholder="password">
-	<label>re-enter password:</label>
-	<input type="password" name="reenter-password" placeholder="re-enter password">
-	<input type="submit">
-</form>
+	<div class="row">
+		<div class="col-xs-3 col-xs-offset-3">
+			<h3> Create a user here. </h3>
+			{{ Form::open(array('role' => 'form', 'url' => 'users', 'method' => 'POST')) }}
+				<fieldset>
+					<div class="form-group">
+						{{ Form::label('name', 'name') }}
+						{{ Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'user name')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::label('email', 'email') }}
+						{{ Form::email('email', null, array('class' => 'form-control', 'placeholder' => 'email address')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::label('password', 'password') }}
+						{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'password')) }}
+					</div>
+					<div class="form-group">
+						{{ Form::label('reenter-password', 're-enter password') }}
+						{{ Form::password('reenter-pasword', array( 'class' => 'form-control', 'placeholder' => 're-enter password'))}}
+					</div>
+				    <div class="form-group">
+				        {{ Form::submit('Save Issue', array('class'=>'btn btn-primary pull-right')) }}
+				    </div>
+				</fieldset>
+			{{ Form::close() }}
+		</div>
+	</div>
 @stop
