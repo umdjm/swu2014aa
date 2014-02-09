60577e3b6029fdd77f1ed15419265cbfb0371d91
mjeffery <michael.jeffery@gmail.com>
2014-02-08 19:17:59 -0500

    added responsive styling for the users.create route

--- www/app/views/users/create.blade.php
+++ www/app/views/users/create.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-xs-3 col-xs-offset-3">
+		<div class="col-xs-12 col-sm-12 col-lg-3 col-lg-offset-3">
 			<h3> Create a user here. </h3>
 			{{ Form::open(array('role' => 'form', 'url' => 'users', 'method' => 'POST')) }}
 				<fieldset>
 					<div class="form-group">
-						{{ Form::label('name', 'name') }}
+						{{ Form::label('name', 'name', array('class' => 'hidden-xs hidden-sm')) }}
 						{{ Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'user name')) }}
 					</div>
 					<div class="form-group">
-						{{ Form::label('email', 'email') }}
+						{{ Form::label('email', 'email', array('class' => 'hidden-xs hidden-sm')) }}
 						{{ Form::email('email', null, array('class' => 'form-control', 'placeholder' => 'email address')) }}
 					</div>
 					<div class="form-group">
-						{{ Form::label('password', 'password') }}
+						{{ Form::label('password', 'password', array('class' => 'hidden-xs hidden-sm'))}}
 						{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'password')) }}
 					</div>
 					<div class="form-group">
-						{{ Form::label('reenter-password', 're-enter password') }}
+						{{ Form::label('reenter-password', 're-enter password', array('class' => 'hidden-xs hidden-sm')) }}
 						{{ Form::password('reenter-pasword', array( 'class' => 'form-control', 'placeholder' => 're-enter password'))}}
 					</div>
 				    <div class="form-group">
