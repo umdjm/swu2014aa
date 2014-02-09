caeb2a8c7fa52f7fc6e9b57032a24c92b28446aa
mjeffery <michael.jeffery@gmail.com>
2014-02-09 11:21:43 -0500

    started the follow from index

--- www/app/views/partials/indexes/issues.blade.php
+++ www/app/views/partials/indexes/issues.blade.php
@@ -7,13 +7,18 @@
               </a>
       	</article>
       </li>
-    @foreach ($issues as $issue)
+    @foreach ($issues as $index=>$issue)
       <li data-count="2" data-totalcount="12" class="grid-block pill">
       	<article class="grid-idea">
               <a class="cover" href="/issues/{{ $issue->id }}">
 
                   <div class="circleBase idea-status status-{{ $issue->priority_string() }}" >
                   </div>
+                  <!--
+                  <div class="idea-following">
+                    <i class="fa fa-star-o"></i>
+                  </div>
+                  -->
                   <h1 class="important h4 overlay issue-name" >{{ $issue->name }}</h1>
       			<img src="{{ $issue->photo }}" class="pill grid-img photo" alt="IdeaPot" width="1000px" height="1000px">
       			<div class="pill overlay description">
