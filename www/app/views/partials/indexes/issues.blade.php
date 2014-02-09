f1c6b90fdada9ea457ada09c3a6a85487865072e
David H Hagan <david@davidhhagan.com>
2014-02-09 13:39:24 -0500

    lowered header on images

--- www/app/views/partials/indexes/issues.blade.php
+++ www/app/views/partials/indexes/issues.blade.php
@@ -4,8 +4,20 @@
             
     @foreach ($issues as $index=>$issue)
       <li class="square-issue" style="background-image:url({{ $issue->photo }});" onclick="location.href = '/issues/{{ $issue->id }}';">
-       <!-- <div class="circleBase idea-status status-{{ $issue->priority_string() }}" ></div>-->
-        <h1 class="important h4 overlay issue-name" >{{ $issue->name }}</h1>
+        <div class="circleBase idea-status status-{{ $issue->priority_string() }}" ></div>
+       <div class="image-label">
+        <h1>{{ $issue->name }}</h1>
+
+
+<!--
+        <div class="pill overlay description">
+            Priority:{{ $issue->priority_string()}}
+            <p>{{ $issue->desc }}</p>
+        </div>
+
+-->
+
+      </div>
       </li>
 
       <!--<li class="square-issue thumbnail"><a class="cover" href="/issues/{{ $issue->id }}"><img src="{{ $issue->photo }}" alt="sq-image" /></a></li>-->
@@ -15,3 +27,4 @@
   </ul>
 
 </div><!-- everything that goes in da block o' blocks -->
+
